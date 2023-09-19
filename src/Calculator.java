f29cdced61ff8cd1d615f804a8c181a10c553635
Unberger <107785779+Unberger@users.noreply.github.com>
2023-09-19 14:23:16 +0500

    Fixed an exception
    Deleted try/catch, keep throw

--- src/Calculator.java
+++ src/Calculator.java
@@ -23,62 +23,56 @@ public class Calculator {
                 operIndex = i;
             }
         }
-        try {
+        if (operIndex == -1) {
+            throw new IllegalArgumentException(NOT_MATH_OPER);
+        }
+        for (int i = 0; i < oper.length; i++) {
+            operCount += viraz.length() - viraz.replace(String.valueOf(oper3[i]), "").length();
+        }
+        if (operCount > 1) {
+            throw new IllegalArgumentException(NOT_FORMAT_MATH);
+        }
+        if (operCount == 0) {
+            throw new IllegalArgumentException(NOT_MATH_OPER);
+        }
+        String[] num = viraz.split(oper2[operIndex]); //получили 2 числа из выражения
 
-            if (operIndex == -1) {
-                throw new IllegalArgumentException(NOT_MATH_OPER);
-            }
-            for (int i = 0; i < oper.length; i++) {
-                operCount += viraz.length() - viraz.replace(String.valueOf(oper3[i]), "").length();
-            }
-            if (operCount > 1) {
-                throw new IllegalArgumentException(NOT_FORMAT_MATH);
-            }
-            if (operCount == 0) {
-                throw new IllegalArgumentException(NOT_MATH_OPER);
-            }
-            String[] num = viraz.split(oper2[operIndex]); //получили 2 числа из выражения
+        if (roman.isRoman(num[0]) == roman.isRoman(num[1])) { //проверяем является ли операнды из одинаковой системы счисления
+            int a, b, result;
+            boolean isRoman = roman.isRoman(num[0]);
 
-            if (roman.isRoman(num[0]) == roman.isRoman(num[1])) { //проверяем является ли операнды из одинаковой системы счисления
-                int a, b, result;
-                boolean isRoman = roman.isRoman(num[0]);
-
-                if (isRoman) { //проверяем является ли операнды из римской системы счисления
-                    a = roman.romToArab(num[0]);
-                    b = roman.romToArab(num[1]);
-                    if ((operIndex == 1) && (a <= b)) {
-                        throw new IllegalArgumentException(NOT_ROM_OTRIC);
-                    }
-                    if ((0 >= a) | (a > 10) | (0 >= b) | (b > 10)) {
-                        throw new IllegalArgumentException(NOT_SINGLE_DIGITS);
-                    }
-
-                } else { //иначе переводим операнды из выражения - строки в целочисленные
-                    a = Integer.parseInt(num[0]);
-                    b = Integer.parseInt(num[1]);
-                    if ((0 >= a) | (a > 10) | (0 >= b) | (b > 10)) {
-                        throw new IllegalArgumentException(NOT_SINGLE_DIGITS);
-                    }
+            if (isRoman) { //проверяем является ли операнды из римской системы счисления
+                a = roman.romToArab(num[0]);
+                b = roman.romToArab(num[1]);
+                if ((operIndex == 1) && (a <= b)) {
+                    throw new IllegalArgumentException(NOT_ROM_OTRIC);
+                }
+                if ((0 >= a) | (a > 10) | (0 >= b) | (b > 10)) {
+                    throw new IllegalArgumentException(NOT_SINGLE_DIGITS);
                 }
 
-                result = switch (oper[operIndex]) { //производим вычисления с помощью перебора вариантов
-                    case "+" -> a + b;
-                    case "-" -> a - b;
-                    case "*" -> a * b;
-                    default -> a / b;
-                };
-
-                if (isRoman) {
-                    System.out.println(roman.arabToRom(result));
-                } else {
-                    System.out.println(result);
+            } else { //иначе переводим операнды из выражения - строки в целочисленные
+                a = Integer.parseInt(num[0]);
+                b = Integer.parseInt(num[1]);
+                if ((0 >= a) | (a > 10) | (0 >= b) | (b > 10)) {
+                    throw new IllegalArgumentException(NOT_SINGLE_DIGITS);
                 }
+            }
+
+            result = switch (oper[operIndex]) { //производим вычисления с помощью перебора вариантов
+                case "+" -> a + b;
+                case "-" -> a - b;
+                case "*" -> a * b;
+                default -> a / b;
+            };
+
+            if (isRoman) {
+                System.out.println(roman.arabToRom(result));
             } else {
-                throw new IllegalArgumentException(NOT_TWO_SS);
+                System.out.println(result);
             }
-
-        } catch (IllegalArgumentException ex) {
-            ex.printStackTrace();
+        } else {
+            throw new IllegalArgumentException(NOT_TWO_SS);
         }
     }
 }
